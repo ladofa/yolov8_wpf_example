40e37a5a8dbae9a9783b90112fb9c6b36c02391d
ladofa <ladofa9@gmail.com>
2023-09-02 23:06:20 +0900

    Add project files.

---
+++ wpf_ml/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_yolov8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void LoadModelButton_Click(object sender, RoutedEventArgs e)
+        {
+            //SessionOptions gpuOption = new SessionOptions();
+            //gpuOption.AppendExecutionProvider_DML(0);
+            Yolov8.LoadOnnxSession("yolov8n (5).onnx");
+        }
+
+        OpenCvSharp.Mat mainMat;
+
+        private void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            mainMat = OpenCvSharp.Cv2.ImRead(openFileDialog.FileName);
+            var source = OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(mainMat);
+            MainImage.Source = source;
+        }
+
+        private void ProcessModelButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<float[]> results = Yolov8.RunOnnxSesion(mainMat);
+
+            Point relativeLocation = MainImage.TranslatePoint(new Point(0, 0), MainGrid);
+            double marginWidth = relativeLocation.X;
+            double marginHeight = relativeLocation.Y;
+
+            var W = MainImage.ActualWidth;
+            var H = MainImage.ActualHeight;
+
+            ResultGrid.Children.Clear();
+
+            foreach (float[] box in results)
+            {
+                var x1 = box[0] / 640 * W;
+                var y1 = box[1] / 640 * H;
+                var x2 = box[2] / 640 * W;
+                var y2 = box[3] / 640 * H;
+                var w = x2 - x1;
+                var h = y2 - y1;
+
+                var rect = new Rectangle()
+                {
+                    Width = w,
+                    Height = h,
+                    Margin = new Thickness(x1 + marginWidth, y1 + marginHeight, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 3,
+                };
+
+                ResultGrid.Children.Add(rect);
+            }
+        }
+    }
+}
---
+++ wpf_ml/Yolov8.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+
+namespace wpf_yolov8
+{
+    public static class Yolov8
+    {
+        static InferenceSession session;
+        public static void LoadOnnxSession(string path)
+        {
+            session = new InferenceSession(path);
+        }
+
+        public static List<float[]> RunOnnxSesion(Mat input)
+        {
+            var mat = CvDnn.BlobFromImage(input, 1.0 / 255.0, new OpenCvSharp.Size(640, 640), swapRB: true, crop: false);
+            float[] mem = new float[3 * 640 * 640];
+            System.Runtime.InteropServices.Marshal.Copy(mat.DataStart, mem, 0, 640 * 640 * 3);
+            var tensor = new DenseTensor<float>(mem, new[] { 1, 3, 640, 640 });
+
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor<float>("images", tensor)
+            };
+
+            using (var output = session.Run(inputs))
+            {
+                var result = output.ElementAt(0).Value as DenseTensor<float>;
+
+                if (result == null)
+                {
+                    return new List<float[]>();
+                }
+                var arr = result.ToArray();
+                return postProcessing(arr);
+            }
+        }
+
+        static List<float[]> postProcessing(float[] arr, int cat = 80, float thres = 0.3f)
+        {
+            int n = 8400;
+            float[] maxProb = new float[n];
+            float[] maxIndex = new float[n];
+
+            List<float[]> boxes = new List<float[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                float M = 0;
+                float index = 0;
+                for (int j = 0; j < cat; j++)
+                {
+                    var prob = arr[(j + 4) * n + i];
+                    if (prob > M)
+                    {
+                        M = prob;
+                        index = j;
+                    }
+                }
+
+                if (M > 0.5)
+                {
+                    float cx = arr[0 * n + i];
+                    float cy = arr[1 * n + i];
+                    float w2 = arr[2 * n + i] / 2;
+                    float h2 = arr[3 * n + i] / 2;
+                    float x1 = cx - w2;
+                    float y1 = cy - h2;
+                    float x2 = cx + w2;
+                    float y2 = cy + h2;
+                    float[] box = new float[6]
+                    {
+                        x1, y1, x2, y2, M, index
+                    };
+                    boxes.Add(box);
+                }
+            }
+            var sorted = boxes.OrderByDescending(x => x[5]);
+
+            float iou(float[] box, float[] BOX)
+            {
+                float x1 = box[0], y1 = box[1], x2 = box[2], y2 = box[3];
+                float X1 = BOX[0], Y1 = BOX[1], X2 = BOX[2], Y2 = BOX[3];
+                float ix1 = Math.Max(x1, X1);
+                float ix2 = Math.Min(x2, X2);
+                float iy1 = Math.Max(y1, Y1);
+                float iy2 = Math.Min(y2, Y2);
+                float iw = ix2 - ix1;
+                float ih = iy2 - iy1;
+                if (iw < 0 || ih < 0)
+                    return 0;
+                float inter = (ix2 - ix1) * (iy2 - iy1);
+
+                float area = (x2 - x1) * (y2 - y1);
+                float AREA = (X2 - X1) * (Y2 - Y1);
+                float union = area + AREA - inter;
+                return inter / union;
+            };
+
+            List<float[]> result = new List<float[]>();
+
+            foreach (var candi in sorted)
+            {
+                bool found = false;
+                foreach(var r in result)
+                {
+                    if (iou(r, candi) > 0.7)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                
+                if (!found)
+                {
+                    result.Add(candi);
+                }
+            }
+
+            return result;
+        }
+    }
+}
